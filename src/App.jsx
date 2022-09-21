c920bb48f719bfbd0117038944a018d2ab8dfbcb
rzilla92 <108125593+rzilla92@users.noreply.github.com>
2022-09-21 15:12:09 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Under Construction</h1>
+      <p className="read-the-docs">
+        Check back soon!
+      </p>
+    </div>
+  )
+}
+
+export default App
