03f7b7c4f8550e0fe37ade33ea976df2f0e6db2b
rzilla92 <108125593+rzilla92@users.noreply.github.com>
2022-09-28 14:12:00 +0700

    added buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Projects from './components/Projects'
 import ParticlesBg from 'particles-bg'
 import './App.css'
+import { FaGithub, FaFileAlt } from "react-icons/fa"
 
 function App() {
   return (
@@ -9,7 +10,12 @@ function App() {
       <ParticlesBg className="particles" color="#1DB4E0" type="cobweb" bg={true}/>
       <h1>Under Construction</h1>
       <p className="read-the-docs">
-        Check out some of my projects in the mean time!
+        View my <br />
+         <div className='viewmy'>
+          <a href='https://drive.google.com/file/d/1i0Csl3zy_7wAx6X463IYWuqK1sCKasCZ/view?usp=sharing'> <button><FaFileAlt className='icon'/>CV</button> </a>
+          <a href='https://github.com/rzilla92'> <button><FaGithub className='icon' />Github</button> </a>
+         </div>
+        Or check out some of my projects in the mean time!
       </p>
       <Projects />
     </div>
