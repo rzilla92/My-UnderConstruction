f6facd5a13a2ae6a73e77dad98a96ff672fd4493
rzilla92 <108125593+rzilla92@users.noreply.github.com>
2022-09-28 13:17:13 +0700

    added particles  bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import Projects from './components/Projects'
-
+import ParticlesBg from 'particles-bg'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg className="particles" color="#1DB4E0" type="cobweb" bg={true}/>
       <h1>Under Construction</h1>
       <p className="read-the-docs">
         Check out some of my projects in the mean time!
